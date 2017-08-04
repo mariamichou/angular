9a1b2c8ce7919fc276fcb9aaa3eb07c2d63c795d
Maria Michou <maria.michou@leotech.com.sg>
2017-08-04 17:39:56 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    styleUrls: ['./app.component.css'],
+    providers: [HeroService]
 })
+
 export class AppComponent {
-  title = 'app';
+    title = 'Tour of Heroes';
 }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-heroes',
+  styleUrls: ['./heroes.component.css'],
+  templateUrl: './heroes.component.html',
+  providers: []
+})
+
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService, private router: Router) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+}
